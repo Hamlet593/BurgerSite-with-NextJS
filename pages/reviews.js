1bc8fef88b0d323d9446f24c0dda78a933ebc225
Hamo Muradyan <hamomuradyan93@gmail.com>
2022-08-12 17:40:51 +0400

    fetch the data

---
+++ pages/reviews.js
@@ -0,0 +1,40 @@
+import Head from 'next/head';
+
+// export default function Hamo (props) {
+//     console.log(props);
+//     return (
+//         <div>69</div>
+//     )
+// }
+// Hamo();
+
+export default function Reviews ({reviews}) {
+    console.log(reviews)
+    return (
+        <>
+            <Head>
+                <title>Reviews</title>
+            </Head>
+            <h1>Our reviews</h1>
+            <div className='reviews'>
+                {!!reviews && reviews.map(({id, body}) => {
+                    return (
+                        <div key={id} className='review'>
+                            {id} {body.slice(0, 40)}
+                        </div>
+                    )
+                })}
+            </div>
+        </>
+    )
+}
+
+export async function getServerSideProps() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/comments');
+    const data = await response.json();
+    return {
+        props: {
+            reviews: data.slice(0, 20)
+        }
+    }
+}
