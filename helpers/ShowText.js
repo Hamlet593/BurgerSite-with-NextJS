a14541e37e23aab9241d523c6ea3c0a46ebd274d
Hamo Muradyan <hamomuradyan93@gmail.com>
2022-08-23 19:12:47 +0400

    Adding showText component for text

---
+++ helpers/ShowText.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import { useState } from "react";
+
+const ShowText = ({ text, max = 80 }) => {
+    const [more, setMore] = useState(false);
+
+    return (
+        <>
+            {text.length <= max ?
+                text :
+                !more ?
+                    <div>{text.slice(0, max)} <Link href='#'><a onClick={evt => {
+                        evt.preventDefault();
+                        setMore(true)
+                    }}>... more</a></Link></div> :
+                    <div>{text} <Link href='f'><a onClick={evt => {
+                        evt.preventDefault();
+                        setMore(false)
+                    }}>less</a></Link></div>}
+        </>
+    )
+}
+
+export default ShowText;
--- pages/todoList.js
+++ pages/todoList.js
@@ -1,14 +1,21 @@
-import { useState } from "react";
+import ShowText from "../helpers/ShowText";
 
 const TodoList = () => {
-    let text = `At All About Burger - we know how a burger should taste! We pride ourselves in making fresh, never-frozen beef that is locally sourced and ground fresh daily. It doesn't stop there... Our bread is baked fresh daily, our onion rings are fried fresh to order, and our fries are cooked by the order from freshly sliced potatoes! We do EVERYTHING from A to Z!.`
-    const [more, setMore] = useState(false);
     return (
         <>
-            {!more ? <div>{text.slice(0, 110)} <a onClick={() => setMore(true)}>... more</a></div>
-                : <div>{text} <a onClick={() => setMore(false)}>less</a></div>}
-        </>
-    )
-}
+            <ShowText
+                text={`${`These our burgers.`}`}
+            />
+            <br />
+            <ShowText
+                text={`${`At All About Burger - we know how a burger should taste! We pride ourselves in making fresh, never-frozen beef that is locally sourced and ground fresh daily. It doesn't stop there... Our bread is baked fresh daily, our onion rings are fried fresh to order, and our fries are cooked by the order from freshly sliced potatoes! We do EVERYTHING from A to Z!.`}`}
+                max={20}
+            />
+            <br />
+            <ShowText
+                text={`${`At All About Burger - we know how a burger should taste! We pride ourselves in making fresh, never-frozen beef that is locally sourced and ground fresh daily. It doesn't stop there... Our bread is baked fresh daily, our onion rings are fried fresh to order, and our fries are cooked by the order from freshly sliced potatoes! We do EVERYTHING from A to Z!.`}`}
+            />
+        </>)
+};
 
 export default TodoList;
